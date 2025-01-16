5d9a33ad21301ba84ebb686a33e7d5b6ffe5e526
lixiuxia <xiuxia.li@dstp.com>
2025-01-16 15:46:41 +0800

    添加退出登录

--- src/views/my/my.tsx
+++ src/views/my/my.tsx
@@ -1,12 +1,16 @@
 import "~/assets/css/my.less";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import Tabbar from "~/components/tabbar";
 import { useTypedSelector } from "~/hook/hook";
 import { Cell } from "react-vant";
-
+import { clearUserMessStore } from "~/store/modules/userMess";
+import { clearHeaderStore } from "~/store/modules/header";
+import { clearSerialNoStore } from "~/store/modules/serialNo";
 import Img from "../../assets/images/my/profile_touxiang@2x.png";
 export default function My() {
   const navigate = useNavigate();
+  let dispatch = useDispatch();
   const UserStore = useTypedSelector((state) => state.userMessSlice);
   const HeaderStore = useTypedSelector((state) => state.headerSlice);
   console.log("UserStore", UserStore);
@@ -24,7 +28,12 @@ export default function My() {
   const goServer = () => {
     navigate("/CustomerService");
   };
-
+  const exit = () => {
+    dispatch(clearUserMessStore());
+    dispatch(clearHeaderStore());
+    dispatch(clearSerialNoStore());
+    console.log("退出");
+  };
   return (
     <div className="pageBg my">
       <div className="company">
@@ -51,6 +60,12 @@ export default function My() {
             title="联系客服"
             icon={<span className="span span2"></span>}
             onClick={goServer}
+            isLink
+          ></Cell>
+          <Cell
+            title="退出登录"
+            icon={<span className="span span1"></span>}
+            onClick={exit}
             isLink
           ></Cell>
         </div>
